93acd2c8e0337bedcc60fb8f4f55d7a549e6f862
Edson Silva <edson@purespectrum.com>
2021-05-19 16:56:28 -0300

    Added class and string interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PrimaryMenuComponent } from './primaryMenu/primaryMenu.component';
 import {ServerComponent} from './server/server.component'
+import { SuccessAllert } from './successAlert/success.component';
+import { WarningAlert } from './warningAlert/warning.components';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    PrimaryMenuComponent
+    PrimaryMenuComponent,
+    SuccessAllert,
+    WarningAlert
   ],
   imports: [
     BrowserModule,
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,4 +6,10 @@ import { Component } from "@angular/core";
     styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+    serverId: number = 123456789;
+    serverStatus: boolean = true;
+
+    getServerStatus(){
+        return this.serverStatus === true ? 'online' : 'offline';
+    }
 }
