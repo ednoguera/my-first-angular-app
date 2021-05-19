d641634d336f1d728d6237ae29b9651085e91255
Edson Silva <edson@purespectrum.com>
2021-05-19 12:29:53 -0300

    commiting all necessary files

---
+++ src/app/primaryMenu/primaryMenu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'primary-menu',
+    templateUrl: './primaryMenu.component.html',
+    styleUrls: ['./primaryMenu.component.css']
+})
+export class PrimaryMenuComponent {
+}
